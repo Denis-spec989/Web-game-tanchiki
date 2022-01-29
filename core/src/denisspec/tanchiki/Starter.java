77fd4ff5c278b09744f4e9be3d2bc674774a2e68
Denis Ignatov <dnoevgen@gmail.com>
2022-01-29 22:37:37 +0300

    handling the panzer

--- core/src/denisspec/tanchiki/Starter.java
+++ core/src/denisspec/tanchiki/Starter.java
@@ -1,31 +1,55 @@
 package denisspec.tanchiki;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
+import org.graalvm.compiler.loop.MathUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Starter extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	private Panzer me;
+	private KeyBoardAdapter inputProcessor = new KeyBoardAdapter();
+	private List<Panzer> enemies = new ArrayList<>();
+
+
 	@Override
 	public void create () {
+		Gdx.input.setInputProcessor(inputProcessor);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		me = new Panzer(100,200);
+		List<Panzer> newEnemies =  IntStream.range(0,15).mapToObj(i->{
+			int x= MathUtils.random(Gdx.graphics.getWidth());
+			int y= MathUtils.random(Gdx.graphics.getHeight());
+			return new Panzer(x,y,"panzer_enemy.png");
+		}).collect(Collectors.toList());
+		enemies.addAll(newEnemies);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		me.moveTo(inputProcessor.getDirection());
+		me.rotateTo(inputProcessor.getMousePos());
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		me.render(batch);
+		enemies.forEach(enemy->{
+			enemy.render(batch);
+			enemy.rotateTo(me.getPosition());
+		});
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		me.dispose();
 	}
 }
